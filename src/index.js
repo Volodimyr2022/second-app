90e13268a87fc527ff2b3b1ab35f19764de7611f
Vladimir <107769314+Volodimyr2022@users.noreply.github.com>
2023-03-30 09:55:28 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Form from './components/Form/Form';
 import User from './components/User/User';
+import Cycles from './components/Cycles/Cycles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <User />
-    <Form />
+    {/* <User /> */}
+    {/* <Form /> */}
+    <Cycles />
   </React.StrictMode>
 );
 
